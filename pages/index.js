fea305bb154ae7c5032e8fd55f9b630c265c70bb
faysal725 <afaysal725@gmail.com>
2024-12-13 17:42:22 +0600

    fetch branch first commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import EventList from "@/components/events/EventList";
-import { getFeaturedEvents } from "@/dummy-data";
+
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,14 +7,13 @@ import Link from "next/link";
 
 export default function Home() {
   
-  const featuredEvents = getFeaturedEvents();
   return (
   
 
       <div
         className=""
       >
-        <EventList items={featuredEvents} />
+        asdfsdf
       </div>
     
   );
