405af061e94de8371c88c106ed0212e4da0d6806
faysal725 <afaysal725@gmail.com>
2024-12-26 16:07:26 +0600

    bug fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import Layout from "@/components/layout/layout";
+
 import "@/styles/globals.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
+    <div>
       <Component {...pageProps} />
-    </Layout>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import EventList from "@/components/events/EventList";
-import { getFeaturedEvents } from "@/helpers/api-utils";
+
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
